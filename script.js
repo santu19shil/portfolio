4fd987660a6f40436278d34995f57b5b908ffe59
Santu Shil <santushil.cse.tcea.2027@gmail.com>
2026-07-01 11:24:49 +0530

    sanu

--- script.js
+++ script.js
@@ -126,15 +126,3 @@ window.addEventListener("scroll", () => {
     }
 
 });
-
-const form = document.querySelector(".contact-form");
-
-form.addEventListener("submit", e => {
-
-    e.preventDefault();
-
-    alert("Thank you! Your message has been received.");
-
-    form.reset();
-
-});
